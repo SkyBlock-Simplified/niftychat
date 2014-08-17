2b199c1169215823567d32f85f9c0567b100e11b
Brian Graham <support@netcoding.net>
2014-08-17 00:03:25 -0400

    Global commands now work properly

--- src/net/netcoding/niftychat/commands/SocialSpy.java
+++ src/net/netcoding/niftychat/commands/SocialSpy.java
@@ -35,11 +35,10 @@ public class SocialSpy extends BukkitCommand {
 			server = NiftyBukkit.getBungeeHelper().getServerName();
 
 			if (args.length > 0) {
-				if (NiftyBukkit.getBungeeHelper().getServer(args[args.length - 1]) != null)
+				if (NiftyBukkit.getBungeeHelper().getServer(args[args.length - 1]) != null) {
 					server = args[args.length - 1];
-
-				if (args.length == 1)
-					playerName = sender.getName();
+					if (args.length == 1) playerName = sender.getName();
+				}
 			}
 		}
 
@@ -52,6 +51,11 @@ public class SocialSpy extends BukkitCommand {
 			profile = NiftyBukkit.getMojangRepository().searchByUsername(playerName)[0];
 		} catch (ProfileNotFoundException pnfe) {
 			this.getLog().error(sender, "Unable to locate the uuid of {{0}}!", playerName);
+			return;
+		}
+
+		if (!sender.getName().equals(profile.getName()) && !this.hasPermissions(sender, "vanish", "others")) {
+			this.getLog().error(sender, "You are not allowed to socialspy other players!");
 			return;
 		}
 

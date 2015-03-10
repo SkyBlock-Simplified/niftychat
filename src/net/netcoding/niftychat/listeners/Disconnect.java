5fe370f50f052ee15e146c0da6d44a75f1c1d48d
Brian Graham <support@netcoding.net>
2015-03-09 23:07:00 -0400

    Don't bother removing cache items on disconnect since they are removed from the repository before this event fires

--- src/net/netcoding/niftychat/listeners/Disconnect.java
+++ src/net/netcoding/niftychat/listeners/Disconnect.java
@@ -1,11 +1,8 @@
 package net.netcoding.niftychat.listeners;
 
-import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.BukkitListener;
 import net.netcoding.niftychat.NiftyChat;
-import net.netcoding.niftychat.cache.UserChatData;
 
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -16,20 +13,14 @@ public class Disconnect extends BukkitListener {
 		super(plugin);
 	}
 
-	private void playerDisconnect(Player player) {
-		UserChatData.removeCache(NiftyBukkit.getMojangRepository().searchByUniqueId(player.getUniqueId()));
-	}
-
 	@EventHandler
 	public void onPlayerKick(PlayerKickEvent event) {
 		event.setLeaveMessage("");
-		this.playerDisconnect(event.getPlayer());
 	}
 
 	@EventHandler
 	public void onPlayerLeave(PlayerQuitEvent event) {
 		event.setQuitMessage("");
-		this.playerDisconnect(event.getPlayer());
 	}
 
 }

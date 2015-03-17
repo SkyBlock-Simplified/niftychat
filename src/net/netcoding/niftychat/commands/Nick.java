0cc400bdb45e31c91bd3bfebafa7159bd2ef8c73
Brian Graham <support@netcoding.net>
2015-03-17 09:10:25 -0400

    Added ability to revoke and grant nicknames Don't allow the console to be nicknamed

--- src/net/netcoding/niftychat/commands/Nick.java
+++ src/net/netcoding/niftychat/commands/Nick.java
@@ -2,10 +2,14 @@ package net.netcoding.niftychat.commands;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.database.factory.ResultCallback;
 import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.minecraft.BungeeServer;
 import net.netcoding.niftybukkit.mojang.MojangProfile;
 import net.netcoding.niftybukkit.mojang.exceptions.ProfileNotFoundException;
 import net.netcoding.niftybukkit.util.RegexUtil;
@@ -13,11 +17,17 @@ import net.netcoding.niftybukkit.util.StringUtil;
 import net.netcoding.niftychat.NiftyChat;
 import net.netcoding.niftychat.cache.Cache;
 import net.netcoding.niftychat.cache.Config;
+import net.netcoding.niftychat.cache.UserChatData;
 
 import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 public class Nick extends BukkitCommand {
+
+	private static final int MINIMUM_CHARS = 3;
+	private static final int MAXIMUM_CHARS = 16;
+	public static final transient Pattern CONTAIN_NICKNAME_CHARS = Pattern.compile("([\\w-])");
+	public static final transient Pattern INVALID_NICKNAME_CHARS = Pattern.compile("([^\\w-])");
 
 	public Nick(NiftyChat plugin) {
 		super(plugin, "nick");
@@ -33,13 +43,19 @@ public class Nick extends BukkitCommand {
 
 		String playerName = (args.length == 2 ? args[0] : sender.getName());
 		String nick = (args.length == 2 ? args[1] : args[0]);
-		String your = (sender.getName() == playerName ? ChatColor.GRAY + "You" : playerName);
-		String has = (sender.getName() == playerName ? "have" : "has");
+		boolean isMe = sender.getName() == playerName;
+		String your = (isMe ? ChatColor.GRAY + "You" : playerName);
+		String has = (isMe ? "have" : "has");
 		String _nick = nick.toLowerCase();
-		boolean off = _nick.matches("^off|clear$");
-		boolean rev = _nick.matches("^revoke|disable$");
-		boolean ena = _nick.matches("^grant|allow|enable$");
-		MojangProfile profile;
+		boolean clear = _nick.matches("^off|clear$");
+		boolean revoke = _nick.matches("^revoke|disable$");
+		boolean grant = _nick.matches("^grant|allow|enable$");
+		MojangProfile profile = null;
+
+		if (isConsole(playerName)) {
+			this.getLog().error(sender, "You cannot nickname the console!");
+			return;
+		}
 
 		try {
 			profile = NiftyBukkit.getMojangRepository().searchByUsername(playerName);
@@ -48,7 +64,9 @@ public class Nick extends BukkitCommand {
 			return;
 		}
 
-		if (off) {
+		UserChatData userData = UserChatData.getCache(profile);
+
+		if (clear) {
 			if (!playerName.equalsIgnoreCase(sender.getName())) {
 				if (!this.hasPermissions(sender, "nick", "clear")) {
 					this.getLog().error(sender, "You do not have access to clear other players nicknames!");
@@ -57,9 +75,14 @@ public class Nick extends BukkitCommand {
 			}
 
 			nick = null;
-		} else if (rev || ena) {
+		} else if (revoke) {
 			if (!this.hasPermissions(sender, "nick", "revoke")) {
-				this.getLog().error(sender, "You do not have access to grant/revoke players nicknames!");
+				this.getLog().error(sender, "You do not have access to revoke player nickname access!");
+				return;
+			}
+		} else if (grant) {
+			if (!this.hasPermissions(sender, "nick", "grant")) {
+				this.getLog().error(sender, "You do not have access to grant player nickname access!");
 				return;
 			}
 		} else {
@@ -70,45 +93,49 @@ public class Nick extends BukkitCommand {
 				}
 			}
 
-			String noColorNick  = RegexUtil.strip(nick, RegexUtil.REPLACE_ALL_PATTERN);
-			String strippedNick = RegexUtil.strip(noColorNick, RegexUtil.INVALID_NICKNAME_CHARZ);
-
-			if (strippedNick != noColorNick) {
-				this.getLog().error(sender, "Nicknames can only contain letters, numbers and underscores!");
+			if (userData.getFlagData("nick-revoke").getValue()) {
+				this.getLog().message(sender, "{{0}} has nickname access revoked!", profile.getName());
 				return;
 			}
 
-			if (!this.hasPermissions(sender, "nick", "length")) {
-				int minChars = 3;
-				int maxChars = 16;
+			String noColorNick  = RegexUtil.strip(nick, RegexUtil.REPLACE_ALL_PATTERN);
+			String strippedNick = RegexUtil.strip(noColorNick, INVALID_NICKNAME_CHARS);
 
-				if (strippedNick.length() < minChars) {
-					this.getLog().error(sender, "Nicknames must contain at least {{0}} letters!", minChars);
+			if (strippedNick != noColorNick) {
+				if (!this.hasPermissions(sender, "nick", "special")) {
+					this.getLog().error(sender, "Nicknames can only contain letters, numbers and underscores!");
+					return;
+				}
+			}
+
+			if (!this.hasPermissions(sender, "nick", "length")) {
+				if (strippedNick.length() < MINIMUM_CHARS) {
+					this.getLog().error(sender, "Nicknames must contain at least {{0}} letters!", MINIMUM_CHARS);
 					return;
 				}
 
-				if (strippedNick.length() > maxChars) {
-					this.getLog().error(sender, "Nicknames can only be 16 characters long!", minChars);
+				if (strippedNick.length() > MAXIMUM_CHARS) {
+					this.getLog().error(sender, "Nicknames can only be {{0}} characters long!", MAXIMUM_CHARS);
 					return;
 				}
 			}
 
 			if (RegexUtil.replaceColor(nick, RegexUtil.REPLACE_COLOR_PATTERN) != nick) {
-				if (!this.hasPermissions(sender, false, "nick", "color")) {
+				if (!this.hasPermissions(sender, "nick", "color")) {
 					this.getLog().error(sender, "You do not have access to use color in nicknames!");
 					return;
 				}
 			}
 
 			if (RegexUtil.replaceColor(nick, RegexUtil.REPLACE_MAGIC_PATTERN) != nick) {
-				if (!this.hasPermissions(sender, false, "nick", "magic")) {
+				if (!this.hasPermissions(sender, "nick", "magic")) {
 					this.getLog().error(sender, "You do not have access to use magic nicknames!");
 					return;
 				}
 			}
 
 			if (RegexUtil.replaceColor(nick, RegexUtil.REPLACE_FORMAT_PATTERN) != nick) {
-				if (!this.hasPermissions(sender, false, "nick", "format")) {
+				if (!this.hasPermissions(sender, "nick", "format")) {
 					this.getLog().error(sender, "You do not have access to use formatting in nicknames!");
 					return;
 				}
@@ -127,49 +154,46 @@ public class Nick extends BukkitCommand {
 			}
 		}
 
-		if (rev) {
-			//User.disableNick(user);
-		} else if (ena) {
-			//User.enableNick(user);
+		if (revoke) {
+			if (userData.updateFlagData("nick-revoke", true, "*", 0))
+				this.getLog().message(sender, "{{0}} now {1} no nickname access!", your, has);
+		} else if (grant) {
+			if (userData.resetFlagData("nick-revoke", "*"))
+				this.getLog().message(sender, "{{0}} now {1} nickname access.", your, has);
 		} else {
-			try {
-				String _ufnick = null;
-				if (nick != null) _ufnick = RegexUtil.replaceColor(nick, RegexUtil.REPLACE_ALL_PATTERN);
+			String _ufnick = null;
+			if (nick != null) _ufnick = RegexUtil.replaceColor(nick, RegexUtil.REPLACE_ALL_PATTERN);
 
-				if (Cache.MySQL.update(StringUtil.format("UPDATE `{0}` SET `nick` = ?, `ufnick` = ? WHERE `uuid` = ?;", Config.USER_TABLE), nick, _ufnick, profile.getUniqueId())) {
-					if (off)
-						this.getLog().message(sender, "{{0}} now {1} no nickname.", your, has);
-					else
-						this.getLog().message(sender, "{{0}} now {1} the nickname {{2}}.", your, has, RegexUtil.replaceColor(nick, RegexUtil.REPLACE_COLOR_PATTERN));
-				} else
-					this.getLog().error(sender, "Player {{0}} not found!", playerName);
-			} catch (Exception ex) {
-				this.getLog().error(sender, "Unable to set nickname {{0}} for {{1}}.", ex, nick, playerName);
-			}
+			if (Cache.MySQL.update(StringUtil.format("UPDATE `{0}` SET `nick` = ?, `ufnick` = ? WHERE `uuid` = ?;", Config.USER_TABLE), nick, _ufnick, profile.getUniqueId())) {
+				if (clear)
+					this.getLog().message(sender, "{{0}} now {1} no nickname.", your, has);
+				else
+					this.getLog().message(sender, "{{0}} now {1} the nickname {{2}}.", your, has, RegexUtil.replaceColor(nick, RegexUtil.REPLACE_COLOR_PATTERN));
+			} else
+				this.getLog().error(sender, "Player {{0}} not found!", playerName);
 		}
 	}
 
-	/*@Override
+	@Override
 	public List<String> onTabComplete(CommandSender sender, String label, String[] args) {
 		final String firstArg = (args.length > 0 ? args[0] : "");
+		List<String> names = new ArrayList<>();
 
-		Iterable<String> iterable = Iterables.transform(Iterables.filter(UserChatData.getCache(), new Predicate<UserChatData>() {
-			@Override
-			public boolean apply(UserChatData userData) {
-				String noColorNick = RegexUtil.strip(userData.getDisplayName(), RegexUtil.VANILLA_PATTERN);
-				noColorNick = noColorNick.replace("*", "").toLowerCase();
-				return noColorNick.startsWith(firstArg) || noColorNick.contains(firstArg);
+		if (NiftyBukkit.getBungeeHelper().isDetected()) {
+			for (BungeeServer server : NiftyBukkit.getBungeeHelper().getServers()) {
+				for (MojangProfile profile : server.getPlayerList()) {
+					if (profile.getName().startsWith(firstArg) || profile.getName().contains(firstArg))
+						names.add(profile.getName());
+				}
 			}
-		}), new Function<UserChatData, String>() {
-			@Override
-			public String apply(UserChatData userData) {
-				String noColorNick = RegexUtil.strip(userData.getDisplayName(), RegexUtil.VANILLA_PATTERN);
-				noColorNick = noColorNick.replace("*", "");
-				return noColorNick;
+		} else {
+			for (UserChatData userData : UserChatData.getCache()) {
+				if (userData.getProfile().getName().startsWith(firstArg) || userData.getProfile().getName().contains(firstArg))
+					names.add(userData.getProfile().getName());
 			}
-		});
+		}
 
-		return iterableToList(iterable.iterator());
-	}*/
+		return names;
+	}
 
 }

6d7e78c1f5a623cc8d9e726895ed9e042a2172d5
Brian Graham <support@netcoding.net>
2015-03-27 18:51:12 -0400

    Use DataUtil instead of ByteStreams

--- src/net/netcoding/niftychat/listeners/MessageReceived.java
+++ src/net/netcoding/niftychat/listeners/MessageReceived.java
@@ -3,6 +3,7 @@ package net.netcoding.niftychat.listeners;
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.BukkitHelper;
 import net.netcoding.niftybukkit.minecraft.BungeeListener;
+import net.netcoding.niftybukkit.util.DataUtil;
 import net.netcoding.niftychat.cache.Config;
 import net.netcoding.niftychat.cache.UserChatData;
 import net.netcoding.niftychat.commands.Message;
@@ -11,7 +12,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.google.common.io.ByteArrayDataInput;
-import com.google.common.io.ByteStreams;
 
 public class MessageReceived extends BukkitHelper implements BungeeListener {
 
@@ -21,12 +21,12 @@ public class MessageReceived extends BukkitHelper implements BungeeListener {
 
 	@Override
 	public void onMessageReceived(String channel, Player player, byte[] message) throws Exception {
-		ByteArrayDataInput input = ByteStreams.newDataInput(message);
+		ByteArrayDataInput input = DataUtil.newDataInput(message);
 		String subChannel = input.readUTF();
 		if (!subChannel.equals(Config.CHAT_CHANNEL)) return;
 		byte[] data = new byte[input.readShort()];
 		input.readFully(data);
-		ByteArrayDataInput fwData = ByteStreams.newDataInput(data);
+		ByteArrayDataInput fwData = DataUtil.newDataInput(data);
 		String action = fwData.readUTF();
 
 		if (action.equals("Message")) {

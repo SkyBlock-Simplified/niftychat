6987c31a624ef4cc78004ff0bf87e76f97492701
Brian Graham <support@netcoding.net>
2015-03-28 10:45:22 -0400

    Fixed async order

--- src/net/netcoding/niftychat/NiftyChat.java
+++ src/net/netcoding/niftychat/NiftyChat.java
@@ -99,23 +99,23 @@ public class NiftyChat extends BukkitPlugin {
 
 	private boolean setupTables() {
 		try {
-			getSQL().createTableAsync(Config.FORMAT_TABLE, "rank VARCHAR(50) NOT NULL PRIMARY KEY, _group VARCHAR(255), _prefix VARCHAR(255), _suffix VARCHAR(255), _message VARCHAR(50), _format VARCHAR(255)");
-			getSQL().createTableAsync(Config.CENSOR_TABLE, "badword VARCHAR(255) NOT NULL PRIMARY KEY, _replace VARCHAR(255)");
-			getSQL().createTableAsync(Config.USER_TABLE, "uuid VARCHAR(37) NOT NULL PRIMARY KEY, nick VARCHAR(255), ufnick VARCHAR(16) UNIQUE");
-			getSQL().createTableAsync(Config.USER_FLAGS_TABLE, StringUtil.format("uuid VARCHAR(37) NOT NULL, flag VARCHAR(50) NOT NULL, _value BIT(1) NOT NULL, server VARCHAR(100) NOT NULL, _submitted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, _expires TIMESTAMP NULL, PRIMARY KEY (uuid, flag, server), FOREIGN KEY (uuid) REFERENCES {0}(uuid) ON DELETE CASCADE", Config.USER_TABLE));
-			getSQL().createTableAsync(Config.SERVER_FLAGS_TABLE, "server VARCHAR(100) NOT NULL, flag VARCHAR(50) NOT NULL, _value BIT(1) NOT NULL, PRIMARY KEY (server, flag)");
+			getSQL().createTable(Config.FORMAT_TABLE, "rank VARCHAR(50) NOT NULL PRIMARY KEY, _group VARCHAR(255), _prefix VARCHAR(255), _suffix VARCHAR(255), _message VARCHAR(50), _format VARCHAR(255)");
+			getSQL().createTable(Config.CENSOR_TABLE, "badword VARCHAR(255) NOT NULL PRIMARY KEY, _replace VARCHAR(255)");
+			getSQL().createTable(Config.USER_TABLE, "uuid VARCHAR(37) NOT NULL PRIMARY KEY, nick VARCHAR(255), ufnick VARCHAR(16) UNIQUE");
+			getSQL().createTable(Config.USER_FLAGS_TABLE, StringUtil.format("uuid VARCHAR(37) NOT NULL, flag VARCHAR(50) NOT NULL, _value BIT(1) NOT NULL, server VARCHAR(100) NOT NULL, _submitted TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP, _expires TIMESTAMP NULL, PRIMARY KEY (uuid, flag, server), FOREIGN KEY (uuid) REFERENCES {0}(uuid) ON DELETE CASCADE", Config.USER_TABLE));
+			getSQL().createTable(Config.SERVER_FLAGS_TABLE, "server VARCHAR(100) NOT NULL, flag VARCHAR(50) NOT NULL, _value BIT(1) NOT NULL, PRIMARY KEY (server, flag)");
 			getSQL().updateAsync(StringUtil.format("INSERT IGNORE INTO {0} (rank, _prefix, message, format) VALUES (?, ?, ?, ?);", Config.FORMAT_TABLE), "default", "&7", "&7", "{displayname} &8>&r {msg}");
 			getSQL().updateAsync(StringUtil.format("INSERT IGNORE INTO {0} (rank, message, format) VALUES (?, ?, ?);", Config.FORMAT_TABLE), "message", "&7", "{sender} &8->&r {receiver} &8>&r {pmsg}");
 
 			if (getSQL().checkColumnExists(Config.CENSOR_TABLE, "replace")) {
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `replace` _replace VARCHAR(255);", Config.CENSOR_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `value` _value BIT(1) NOT NULL;", Config.USER_FLAGS_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `value` _value BIT(1) NOT NULL;", Config.SERVER_FLAGS_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `group` _group VARCHAR(255);", Config.FORMAT_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `prefix` _prefix VARCHAR(255);", Config.FORMAT_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `suffix` _suffix VARCHAR(255);", Config.FORMAT_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `message` _message VARCHAR(50);", Config.FORMAT_TABLE));
-				getSQL().update(StringUtil.format("ALTER TABLE {0} CHANGE `format` _format VARCHAR(255);", Config.FORMAT_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `replace` _replace VARCHAR(255);", Config.CENSOR_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `value` _value BIT(1) NOT NULL;", Config.USER_FLAGS_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `value` _value BIT(1) NOT NULL;", Config.SERVER_FLAGS_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `group` _group VARCHAR(255);", Config.FORMAT_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `prefix` _prefix VARCHAR(255);", Config.FORMAT_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `suffix` _suffix VARCHAR(255);", Config.FORMAT_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `message` _message VARCHAR(50);", Config.FORMAT_TABLE));
+				getSQL().updateAsync(StringUtil.format("ALTER TABLE {0} CHANGE `format` _format VARCHAR(255);", Config.FORMAT_TABLE));
 			}
 
 			return true;

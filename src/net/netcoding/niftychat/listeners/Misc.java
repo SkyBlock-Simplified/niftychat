eccc9a27a428084ca67e88c8d3e40e495b30b100
Brian Graham <support@netcoding.net>
2015-03-30 08:45:29 -0400

    Now handles players changing their name without requiring loggout out

--- src/net/netcoding/niftychat/listeners/Misc.java
+++ src/net/netcoding/niftychat/listeners/Misc.java
@@ -2,6 +2,7 @@ package net.netcoding.niftychat.listeners;
 
 import net.netcoding.niftybukkit.NiftyBukkit;
 import net.netcoding.niftybukkit.minecraft.BukkitListener;
+import net.netcoding.niftybukkit.minecraft.events.PlayerNameChangeEvent;
 import net.netcoding.niftybukkit.mojang.MojangProfile;
 import net.netcoding.niftybukkit.mojang.exceptions.ProfileNotFoundException;
 import net.netcoding.niftychat.cache.UserChatData;
@@ -29,6 +30,17 @@ public class Misc extends BukkitListener {
 
 	@EventHandler
 	public void onRankChangeEvent(RankChangeEvent event) {
+		UserChatData userData = UserChatData.getCache(event.getProfile());
+
+		if (userData.getOfflinePlayer().isOnline()) {
+			userData.updateDisplayName();
+			userData.updateTabListName();
+			userData.applyFlagData(Vanish.FLAG);
+		}
+	}
+
+	@EventHandler
+	public void onPlayerNameChange(PlayerNameChangeEvent event) {
 		UserChatData userData = UserChatData.getCache(event.getProfile());
 
 		if (userData.getOfflinePlayer().isOnline()) {

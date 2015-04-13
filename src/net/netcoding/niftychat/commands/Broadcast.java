422963bed843fb152afaf4e3367fa76326eb2740
Brian Graham <support@netcoding.net>
2015-04-13 19:42:58 -0400

    Added broadcast (with network-wide support) and me commands

---
+++ src/net/netcoding/niftychat/commands/Broadcast.java
@@ -0,0 +1,43 @@
+package net.netcoding.niftychat.commands;
+
+import net.netcoding.niftybukkit.NiftyBukkit;
+import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.minecraft.BungeeServer;
+import net.netcoding.niftybukkit.mojang.MojangProfile;
+import net.netcoding.niftybukkit.util.StringUtil;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Broadcast extends BukkitCommand {
+
+	public Broadcast(JavaPlugin plugin) {
+		super(plugin, "broadcast");
+	}
+
+	@Override
+	protected void onCommand(CommandSender sender, String alias, String[] args) throws Exception {
+		String name = StringUtil.format("{0}[{{1}}{0}]", ChatColor.DARK_GRAY, "Broadcast");
+		String message = StringUtil.format("{0} {{1}}", name, StringUtil.implode(" ", args));
+		boolean network = alias.matches("^n(etwork)?b(r(oadcast)?)?$") && NiftyBukkit.getBungeeHelper().isDetected();
+
+		if (network && !this.hasPermissions(sender, "broadcast", "network")) {
+			this.getLog().error(sender, "You cannot send a network broadcast!");
+			return;
+		}
+
+		if (network) {
+			for (BungeeServer server : NiftyBukkit.getBungeeHelper().getServers()) {
+				for (MojangProfile profile : server.getPlayerList())
+					NiftyBukkit.getBungeeHelper().message(profile, message);
+			}
+		} else {
+			this.getLog().message(this.getPlugin().getServer().getConsoleSender(), message);
+
+			for (MojangProfile profile : NiftyBukkit.getBungeeHelper().getPlayerList())
+				this.getLog().message(profile.getOfflinePlayer().getPlayer(), message);
+		}
+	}
+
+}
---
+++ src/net/netcoding/niftychat/commands/Me.java
@@ -0,0 +1,44 @@
+package net.netcoding.niftychat.commands;
+
+import net.netcoding.niftybukkit.NiftyBukkit;
+import net.netcoding.niftybukkit.minecraft.BukkitCommand;
+import net.netcoding.niftybukkit.mojang.MojangProfile;
+import net.netcoding.niftybukkit.util.StringUtil;
+import net.netcoding.niftychat.listeners.Chat;
+
+import org.bukkit.ChatColor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Me extends BukkitCommand {
+
+	public Me(JavaPlugin plugin) {
+		super(plugin, "me");
+		this.setCheckHelp(false);
+	}
+
+	@Override
+	protected void onCommand(CommandSender sender, String alias, String[] args) throws Exception {
+		String name = StringUtil.format("{0}[{{1}}{0}]", ChatColor.DARK_GRAY, "Console");
+		String message = StringUtil.implode(" ", args);
+
+		if (isPlayer(sender)) {
+			Player player = (Player)sender;
+			name = player.getDisplayName();
+
+			if (Chat.check(this, player, message)) {
+				message = Chat.filter(this, player, "me", message);
+				message = Chat.format(this, player, "me", message);
+			} else
+				return;
+		}
+
+		message = StringUtil.format("{0} &7{1}", name, message);
+		this.getLog().message(this.getPlugin().getServer().getConsoleSender(), message);
+
+		for (MojangProfile profile : NiftyBukkit.getBungeeHelper().getPlayerList())
+			this.getLog().message(profile.getOfflinePlayer().getPlayer(), message);
+	}
+
+}
